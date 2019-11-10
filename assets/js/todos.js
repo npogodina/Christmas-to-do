c04e6bdc934f17c8a05665b34d074b5c99316b7c
Nataliya Pogodina <n.pogodina@gmail.com>
2019-11-09 21:27:33 -0800

    Added functionality to delete todos.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,15 @@
 // Check off specific Todos by clicking
 $("li").click(function() {
     $(this).toggleClass("completed");
-})
+});
+
+// Click on X to delete Todos
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
 
 
 /*
